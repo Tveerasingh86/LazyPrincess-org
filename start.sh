7d127587bc6969f96ae4fd60ad96910e867f23cf
Tveerasingh86 <Tveerasingh@outlook.com>
2024-08-17 20:38:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/test1.git /LazyPrincess
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
-fi
-cd /LazyPrincess
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
 
 FROM python:3.10
 WORKDIR /app

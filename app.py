ca47b351639dc7ff4d8eaefca5ea9ef403369b87
Tveerasingh86 <Tveerasingh@outlook.com>
2024-08-17 20:09:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Tech VJ'
-
-
-if __name__ == "__main__":
-    app.run()
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository" 
+  git clone https://github.com/lazyindu/test1.git /LazyPrincess
+else
+  echo "Cloning Custom Repo from $STREAM_REPO "
+  git clone $UPSTREAM_REPO LazyPrincess
+fy
+cd /LazyPrincess
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
